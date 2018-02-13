2826f35492dbdf9d590ba33d15771fdd895f50a8
Rohin Galhotra <rgalhotra@email.arizona.edu>
2018-02-13 08:58:13 -0700

    blargh

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
 //include SDL2 libraries
 #include <SDL.h>
 #include "ppm.h"
-#include "rgbe.h"
 
 //C++ includes
 #include <iostream>
@@ -53,19 +52,12 @@ void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, int x, int y){
 // \param height height of image
 // \param out output data
 //
-void convolution(float* data, int width, int height, float* out){
+void convolution(float* data, int width, int height, float* out, int radius, int type){
 	//Create temp 2D data to hold input
-	float tempData[height][width];
+	float tempData[height][3 * width];
 
 	//Kernel for image filter
-	float kernel[5][5] =
-	{
-		{1.0, 4.0, 7.0, 4.0, 1.0},
-		{4.0, 16.0, 26.0, 16.0, 4.0},
-		{7.0, 26.0, 41.0, 26.0, 7.0},
-		{4.0, 16.0, 26.0, 16.0, 4.0},
-		{1.0, 4.0, 7.0, 4.0, 1.0}
-	};
+	float kernel[2 * radius + 1][2 * radius + 1];
 	/*float kernel[21][21] =
 	{
 		{0.001174, 0.001291,		0.001405,		0.001514,		0.001616,		0.001707,		0.001786,		0.001849,		0.001896,		0.001925,		0.001934,		0.001925,		0.001896,		0.001849,		0.001786,		0.001707,		0.001616,		0.001514,		0.001405,		0.001291,		0.001174},
@@ -93,35 +85,52 @@ void convolution(float* data, int width, int height, float* out){
 
 	//vars used in calculation
 	int i, j, a, offset, k = 0, b = 0;
-	float w, sum = 0.0;
-	offset = 2;
+	float w, sum = 0.0, distance, calculatedEuler = 1.0 / (2.0 * M_PI, powf(radius / 3.0, 2));
+	offset = radius;
 
 	//populate kernel
-	for(i = 0; i < 5; i++){
-		for(j = 0; j < 5; j++){
-			kernel[i][j] = kernel[i][j] * (1.00/ 273.0);
+	for(i = 0; i < (2 * radius + 1); i++){
+		for(j = 0; j < (2 * radius + 1); j++){
+			if(type == 0){
+				kernel[i][j] = 1.0 / ((2.0 * radius + 1.0) * (2.0 * radius + 1.0));
+			} else if (type == 1){
+				distance = (i * i) + (j * j) /(2 * (radius / 3.0) * (radius / 3.0));
+				kernel[i][j] = calculatedEuler * exp(-1.0 * distance);
+				sum = sum + kernel[i][j];
+			} else {
+				//sharpen
+			}
+		}
+	}
+	if(type == 1){
+		for(i = 0; i < (2 * radius + 1); i++){
+			for(j = 0; j < (2 * radius + 1); j++){
+				kernel[i][j] = (1.0 / sum) * kernel[i][j];
+			}
 		}
 	}
 
 	//convert 1D input array to a 2D array
 	for(i = 0; i < height; i++){
-		for(j = 0; j < width; j++){
-			tempData[i][j] = log(data[k] + FLT_MIN);
+		for(j = 0; j < 3 * width; j++){
+			tempData[i][j] = data[k];
 			k++;
 		}
 	}
 
 	//actual convolution
+	//red
 	for(k = 0; k < height; k++){
-		for(a = 0; a < width; a++){
-			for(i = (k - offset); i < (k + offset + 1); i++){
-				for(j = (a - offset); j < (a + offset + 1); j++){
+		for(a = 0; a < width; a = a + 3){
+			for(i = (k - 3 * offset); i < (k + 3 * offset + 3); i = i + 3){
+				for(j = (a - 3 * offset); j <  (a + 3 * offset + 3); j = j + 3){
+
 					//calculate weight for bilinear filter
-					w = tempData[k][a] - tempData[i][j] / 2.0;
-					w = w * w;
-					if(w < 0.0) w = 0.0;
-					else if (w > 1.0) w = 1.0;
-					w = exp(-1.0 * w);
+					// w = tempData[k][a] - tempData[i][j] / 2.0;
+					// w = w * w;
+					// if(w < 0.0) w = 0.0;
+					// else if (w > 1.0) w = 1.0;
+					// w = exp(-1.0 * w);
 
 					//handling boundaries; uses boundary padding through reflection for its boundary condition
 					if(i < 0 && j < 0) sum = sum + w * tempData[i + offset][j + offset] * kernel[i - (k - offset)][j - (a - offset)];
@@ -137,7 +146,77 @@ void convolution(float* data, int width, int height, float* out){
 			}
 			//store summation for each pixel in the pixel location
 			out[b] = sum;
-			b++;
+			b = b + 3;
+
+			//reset sum
+			sum = 0.0;
+		}
+	}
+
+	//green
+	b = 1;
+	for(k = 0; k < height; k++){
+		for(a = 1; a < width; a = a + 3){
+			for(i = (k - 3 * offset + 1); i < (k + 3 * offset + 4); i = i + 3){
+				for(j = (a - 3 * offset + 1); j <  (a + 3 * offset + 4); j = j + 3){
+
+					//calculate weight for bilinear filter
+					// w = tempData[k][a] - tempData[i][j] / 2.0;
+					// w = w * w;
+					// if(w < 0.0) w = 0.0;
+					// else if (w > 1.0) w = 1.0;
+					// w = exp(-1.0 * w);
+
+					//handling boundaries; uses boundary padding through reflection for its boundary condition
+					if(i < 0 && j < 0) sum = sum + w * tempData[i + offset][j + offset] * kernel[i - (k - offset)][j - (a - offset)];
+					else if (i < 0 && j < (width - 1)) sum = sum + w * tempData[i + offset][j] * kernel[i - (k - offset)][j - (a - offset)];
+					else if (i < (height - 1) && j < 0) sum = sum + w * tempData[i][j + offset] * kernel[i - (k - offset)][j - (a - offset)];
+					else if (i < 0 && j > (width - 1)) sum = sum + w * tempData[i + offset][j - offset] * kernel[i - (k - offset)][j - (a - offset)];
+					else if (i > (height - 1) && j < 0) sum = sum + w * tempData[i - offset][j + offset] * kernel[i - (k - offset)][j - (a - offset)];
+					else if (i > (height - 1) && j > (width - 1)) sum = sum + w * tempData[i - offset][j - offset] * kernel[i - (k - offset)][j - (a - offset)];
+					else if (i > (height - 1)) sum = sum + w * tempData[i - offset][j] * kernel[i - (k - offset)][j - (a - offset)];
+					else if (j > (width - 1)) sum = sum + w * tempData[i][j - offset] * kernel[i - (k - offset)][j - (a - offset)];
+					else sum = sum + w * tempData[i][j] * kernel[i - (k - offset)][j - (a - offset)];
+				}
+			}
+			//store summation for each pixel in the pixel location
+			out[b] = sum;
+			b = b + 3;
+
+			//reset sum
+			sum = 0.0;
+		}
+	}
+
+	b = 2;
+	//blue
+	for(k = 0; k < height; k++){
+		for(a = 2; a < width; a = a + 3){
+			for(i = (k - 3 * offset + 2); i < (k + 3 * offset + 5); i = i + 3){
+				for(j = (a - 3 * offset + 2); j <  (a + 3 * offset + 5); j = j + 3){
+
+					//calculate weight for bilinear filter
+					// w = tempData[k][a] - tempData[i][j] / 2.0;
+					// w = w * w;
+					// if(w < 0.0) w = 0.0;
+					// else if (w > 1.0) w = 1.0;
+					// w = exp(-1.0 * w);
+
+					//handling boundaries; uses boundary padding through reflection for its boundary condition
+					if(i < 0 && j < 0) sum = sum + w * tempData[i + offset][j + offset] * kernel[i - (k - offset)][j - (a - offset)];
+					else if (i < 0 && j < (width - 1)) sum = sum + w * tempData[i + offset][j] * kernel[i - (k - offset)][j - (a - offset)];
+					else if (i < (height - 1) && j < 0) sum = sum + w * tempData[i][j + offset] * kernel[i - (k - offset)][j - (a - offset)];
+					else if (i < 0 && j > (width - 1)) sum = sum + w * tempData[i + offset][j - offset] * kernel[i - (k - offset)][j - (a - offset)];
+					else if (i > (height - 1) && j < 0) sum = sum + w * tempData[i - offset][j + offset] * kernel[i - (k - offset)][j - (a - offset)];
+					else if (i > (height - 1) && j > (width - 1)) sum = sum + w * tempData[i - offset][j - offset] * kernel[i - (k - offset)][j - (a - offset)];
+					else if (i > (height - 1)) sum = sum + w * tempData[i - offset][j] * kernel[i - (k - offset)][j - (a - offset)];
+					else if (j > (width - 1)) sum = sum + w * tempData[i][j - offset] * kernel[i - (k - offset)][j - (a - offset)];
+					else sum = sum + w * tempData[i][j] * kernel[i - (k - offset)][j - (a - offset)];
+				}
+			}
+			//store summation for each pixel in the pixel location
+			out[b] = sum;
+			b = b + 3;
 
 			//reset sum
 			sum = 0.0;
@@ -146,56 +225,12 @@ void convolution(float* data, int width, int height, float* out){
 }
 
 //
-// A simple wrapper to open file, write to it, and then close it.
-//
-// \param filename name of file to write to
-// \param width width of image
-// \param height height of image
-// \param data data to be written
-//
-void writeRGBE(char* filename, int width, int height, float* data){
-	FILE* f = fopen(filename,"wb");
-	RGBE_WriteHeader(f, width, height, NULL);
-	RGBE_WritePixels(f, data, width * height);
-	fclose(f);
-}
-
-//
-// A simple wrapper to open file, read from it, and then close it.
-//
-// \param filename name of file to read from
-// \param width width of image
-// \param height height of image
-//
-float* readRGBE(char* filename, int* width, int* height){
-	FILE* f = fopen(filename,"rb");
-	RGBE_ReadHeader(f, width, height, NULL);
-	float* data = new float[3 * *width * *height];
-	RGBE_ReadPixels_RLE(f, data, *width, *height);
-	fclose(f);
-	return data;
-}
-
- //
- // Converts from RGBE formatted data (values between 0 and 1) to PPM (integers 0-255)
- // \param data data to convert
- // \param size size of data
- //
-unsigned char* scaleToPPM(float* data, int size){
-	unsigned char* newData = new unsigned char[size];
-	for(int i = 0; i < size; i++){
-		newData[i] = (unsigned char)round(data[i] * 255);
-	}
-	return newData;
-}
-
-//
 // Tone Maps the HDR data by applying gamma correction
 // \param data data to gamma correct
 // \param gamma value to correct by
 // \param size size of data
 //
-float* toneMap(float* data, float gamma, int size){
+float* toneMap(float* data, float gamma, float gain, float bias, int size){
 	//To store L in
 	float* lumData = new float[size];
 
@@ -221,100 +256,26 @@ float* toneMap(float* data, float gamma, int size){
 		lumData[i] = (1.0 / 61.0) * (20.0 * r + 40.0 * g + b);
 
 		//calcualating L corrected
-		lum2[i] = exp(gamma * log(lumData[i] + FLT_MIN)) - FLT_MIN;
+		lum2[i] = powf((gain * lumData[i] + bias), gamma);
 
 		//calculating scale to correct by
 		scale = lum2[i] / lumData[i];
 
 		//correcting original values and clamping
 		r = r * scale;
-		if(r > 1.0) r = 1.0;
+		if(r > 255) r = 255;
 		else if (r < 0.0) r = 0.0;
 		g = g * scale;
-		if(g > 1.0) g = 1.0;
+		if(g > 255) g = 255;
 		else if (g < 0.0) g = 0.0;
 		b = b * scale;
-		if(b > 1.0) b = 1.0;
+		if(b > 255) b = 255;
 		else if (b < 0.0) b = 0.0;
 
 		//saving corrected values
-		newData[3 * i] = r;
-		newData[(3 * i) + 1] = g;
-		newData[(3 * i) + 2] = b;
-	}
-	return newData;
-}
-
-//
-// Tone Maps the HDR data by applying gamma correction and a bilinear filter
-// \param data data to tone map
-// \param gamma value to correct by
-// \param size size of data
-// \param width width of data
-// \param height height of data
-//
-float* toneMapFiltered(float* data, float gamma, int size, int width, int height){
-	//To store L in
-	float* lumData = new float[size];
-
-	//To store L corrected in
-	float* lum2 = new float[size];
-
-	//output data
-	float* newData = new float[3*size];
-
-	//vars used for calculation
-	float scale, B, S, gam;
-	float* con = new float[size];
-	int i = 0;
-	float r, g, b;
-
-	//First loop, used to load in data and calculate L
-	for(i = 0; i < size; i++){
-		r = data[3 * i];
-		g = data[(3 * i) + 1];
-		b = data[(3 * i) + 2];
-		lumData[i] = (1.0 / 61.0) * (20.0 * r + 40.0 * g + b);
-	}
-
-	//run convolution
-	convolution(lumData, width, height, con);
-
-	//non-functional gamma calculation
-	//gam = log(5.0)/(*(max_element(con, con + size)) - *(min_element(con, con + size)));
-	gam = gamma;
-
-	//second loop, run rest of calcualtions
-	for(i = 0; i < size; i++){
-
-		//Load data once more
-		r = data[3 * i];
-		g = data[(3 * i) + 1];
-		b = data[(3 * i) + 2];
-
-		//pull data from post-convolution
-		B = con[i];
-
-		//perform rest of corrections to data
-		S = log(lumData[i] + FLT_MIN) - B;
-		lum2[i] = exp(gam * B + S) - FLT_MIN;
-
-		//finish calculations for corrected values
-		scale = lum2[i] / lumData[i];
-		r = r * scale;
-		if(r > 1.0) r = 1.0;
-		else if (r < 0.0) r = 0.0;
-		g = g * scale;
-		if(g > 1.0) g = 1.0;
-		else if (g < 0.0) g = 0.0;
-		b = b * scale;
-		if(b > 1.0) b = 1.0;
-		else if (b < 0.0) b = 0.0;
-
-		//store corrected values
-		newData[3 * i] = r;
-		newData[(3 * i) + 1] = g;
-		newData[(3 * i) + 2] = b;
+		newData[3 * i] = round(r);
+		newData[(3 * i) + 1] = round(g);
+		newData[(3 * i) + 2] = round(b);
 	}
 	return newData;
 }
@@ -331,10 +292,9 @@ float* toneMapFiltered(float* data, float gamma, int size, int width, int height
 int main(int argc, char** argv) {
 
 	//vars used for new code--loading in image, perform corrections, display and output.
-	bool filetype, bilinear = false;
-	float gamma = 1.0;
+	float gamma = 1.0, bias = 1.0, gain = 1.0;
 	ppm* image = new ppm();
-	int width, height;
+	int width, height, radius = 1, type = -1;
 	float* data;
 	float* newData;
 
@@ -345,30 +305,15 @@ int main(int argc, char** argv) {
 	}
 
 	//setup for loading image; create new object and check commandline args
-	if(argc < 4){
-		cout << "usage: prog02 input output filetype" << endl;
+	if(argc < 3){
+		cout << "usage: prog02 input output" << endl;
 		exit(EXIT_FAILURE);
 	}
 
 	//Try to figure out if it's a ppm or a hdr image
-	if(strcmp(argv[3],"ppm") == 0 || strcmp(argv[3],"PPM") == 0){
-		filetype = true;
-	} else {
-		//if not ppm, read in data and tone map it
-		data = readRGBE(argv[1], &width, &height);
-		newData = toneMap(data, gamma, width * height);
-		filetype = false;
-	}
 
 	//read in image data if ppm
-	if(filetype){
-	 image->readData(argv[1]);
- } else {
-	 //if not ppm, scale data to PPM and store it in a ppm object
-	 image->setData(scaleToPPM(newData, 3*width*height));
-	 image->setWidth(width);
-	 image->setHeight(height);
- }
+	image->readData(argv[1]);
 
  //create window for the image, then check to make sure it loaded properly
  SDL_Window *windowImage = SDL_CreateWindow("Loaded Image", 100, 100, image->returnWidth(), image->returnHeight(), SDL_WINDOW_SHOWN);
@@ -440,31 +385,56 @@ int main(int argc, char** argv) {
             break;
 					case SDLK_LEFT:
 						//if left arrow pressed, decrease gamma by 0.1 and tone map image again
-						if(!filetype){
-							gamma = gamma - 0.1;
-							if (!bilinear) newData = toneMap(data, gamma, width * height);
-							else newData = toneMapFiltered(data, gamma, width * height, width, height);
-						}
+						gamma = gamma - 0.1;
+						newData = toneMap((float*)image->returnData(), gamma, gain, bias, width * height);
 						break;
 					case SDLK_RIGHT:
 						//same as for left arrow, but for right arrow and increase gamma instead of decrease
-						if(!filetype){
-							gamma = gamma + 0.1;
-							if (!bilinear) newData = toneMap(data, gamma, width * height);
-							else newData = toneMapFiltered(data, gamma, width * height, width, height);
-						}
+						gamma = gamma + 0.1;
+						newData = toneMap((float*)image->returnData(), gamma, gain, bias, width * height);
+						break;
+					case SDLK_DOWN:
+						//if left arrow pressed, decrease gamma by 0.1 and tone map image again
+						gain = gain - 0.1;
+						newData = toneMap((float*)image->returnData(), gamma, gain, bias, width * height);
+						break;
+					case SDLK_UP:
+						//same as for left arrow, but for right arrow and increase gamma instead of decrease
+						gain = gain + 0.1;
+						newData = toneMap((float*)image->returnData(), gamma, gain, bias, width * height);
+						break;
+					case SDLK_a:
+						//if left arrow pressed, decrease gamma by 0.1 and tone map image again
+						bias = bias - 0.1;
+						newData = toneMap((float*)image->returnData(), gamma, gain, bias, width * height);
+						break;
+					case SDLK_d:
+						//same as for left arrow, but for right arrow and increase gamma instead of decrease
+						bias = bias + 0.1;
+						newData = toneMap((float*)image->returnData(), gamma, gain, bias, width * height);
+						break;
+					case SDLK_v:
+						//if left arrow pressed, decrease gamma by 0.1 and tone map image again
+						radius = radius - 1;
+						if(radius < 1) radius = 1;
+						convolution((float*)image->returnData(), image->returnWidth(), image->returnHeight(), newData, radius, type);
 						break;
 					case SDLK_b:
-						//if b pressed, toggle bilinear filtering--if it's not currently, filter it. otherwise, stop.
-						if(!filetype){
-							if(!bilinear){
-								newData = toneMapFiltered(data, gamma, width * height, width, height);
-								bilinear = true;
-							} else {
-								newData = toneMap(data, gamma, width * height);
-								bilinear = false;
-							}
-						}
+						//same as for left arrow, but for right arrow and increase gamma instead of decrease
+						radius = radius + 1;
+						convolution((float*)image->returnData(), image->returnWidth(), image->returnHeight(), newData, radius, type);
+						break;
+					case SDLK_n:
+						//if left arrow pressed, decrease gamma by 0.1 and tone map image again
+						type = type - 1;
+						if(radius < 0) type = 0;
+						convolution((float*)image->returnData(), image->returnWidth(), image->returnHeight(), newData, radius, type);
+						break;
+					case SDLK_m:
+						//same as for left arrow, but for right arrow and increase gamma instead of decrease
+						type = type + 1;
+						if(type > 2) type = 2;
+						convolution((float*)image->returnData(), image->returnWidth(), image->returnHeight(), newData, radius, type);
 						break;
           default:
             break;
@@ -472,16 +442,13 @@ int main(int argc, char** argv) {
       }
     }
 		//if not a ppm, store data in one and update the texture with it
-		if(!filetype){
-			SDL_UpdateTexture(imageTexture, NULL, scaleToPPM(newData, 3*width*height), 3*width);
-			image->setData(scaleToPPM(newData, 3*width*height));
+		SDL_UpdateTexture(imageTexture, NULL, (unsigned char*)newData, 3*image->returnWidth());
 
 			//render loaded texture here
-			renderTexture(imageTexture, rendererImage, 0, 0);
+		renderTexture(imageTexture, rendererImage, 0, 0);
 
 			//Update the screen
-			SDL_RenderPresent(rendererImage);
-		}
+		SDL_RenderPresent(rendererImage);
 
     //Display the frame rate to stdout, as well as current gamma value
     const Uint64 end = SDL_GetPerformanceCounter();
@@ -501,6 +468,7 @@ int main(int argc, char** argv) {
 	SDL_Quit();
 
 	//write data to a SDR ppm
+	image->setData((unsigned char*)newData);
 	image->writeData(argv[2]);
 
 	//clear memory
